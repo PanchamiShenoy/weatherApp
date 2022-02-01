d3b6f410f7af409b29e64719a02e27df9acee41e
PanchamiShenoy <panchamishenoy16@gmail.com>
2022-02-01 18:44:04 +0530

    First commit - weather app home screen UI

---
+++ WeatherApp/LocationCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LocationCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Panchami Shenoy on 01/02/22.
+//
+
+import UIKit
+
+class LocationCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cityWeatherIcon: UIImageView!
+    @IBOutlet weak var cityWeeather: UILabel!
+    @IBOutlet weak var cityTemp: UILabel!
+    @IBOutlet weak var cityTime: UILabel!
+    @IBOutlet weak var cityName: UILabel!
+}
